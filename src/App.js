d00212b098210a510004d24a7cf04fd0dac5eb0e
lajawanti <lajawanti.dhake@gmail.com>
2020-09-21 23:47:39 -0400

    Basic set up

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_Client_ID)
   return (
     <div className="App">
-      
+        <h1>Spotify.........</h1>
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,6 @@
+console.log(process.env.REACT_APP_Client_ID)
+console.log(7 * 100)
+console.log(process.env.REACT_APP_PORT)
+
+console.log(process.env.REACT_APP_API_URL)
+console.log(process.env.REACT_APP_NOT_SECRET_CODE)
