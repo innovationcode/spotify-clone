54bc5d713cd08660720f2d1803f2810954f53186
lajawanti <lajawanti.dhake@gmail.com>
2020-09-21 23:59:44 -0400

    Working on Login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Login from './components/login/Login.js';
 import './App.css';
 
 function App() {
   console.log(process.env.REACT_APP_Client_ID)
   return (
     <div className="App">
-        <h1>Spotify.........</h1>
+        {/* Home page with Spotify logo and login button */}
+        <Login />
     </div>
   );
 }
